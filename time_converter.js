c674de2d46fa68c124d5649c0725a0f73e6f4946
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-07-31 21:18:17 -0300

    finish convert function

--- time_converter.js
+++ time_converter.js
@@ -1,36 +1,42 @@
-const YEAR = 31536000;
-const DAY = 86400;
-const HOUR = 3600;
-const MINUTE = 60;
+const time_unities = { year: 31536000,
+                       day: 86400,
+                       hour: 3600,
+                       minute: 60,
+                       second: 1 }
 
-function solution(seconds) {
-    let year = numberOf(YEAR, 'year', seconds);
-    let day = numberOf(DAY, 'day', year.remainingSeconds);
-    let hour = numberOf(HOUR, 'hour', day.remainingSeconds);
-    let minute = numberOf(MINUTE, 'minute', hour.remainingSeconds);
-    let second = minute.remainingSeconds
-
-    return year.amount + ' ' + year.unit + ' ' +
-           day.amount + ' ' + day.unit + ' ' +
-           hour.amount + ' ' + hour.unit + ' ' +
-           minute.amount + ' ' + minute.unit + ' ' +
-           second;
+function convert(seconds) {
+    let converted_time = '';
+    let time_object = null;
+    for (const unit in time_unities) {
+        time_object = convertTimeUnit(time_unities[unit], unit, seconds);
+        console.log(time_object);
+        if (time_object.amount > 0) {
+            converted_time += time_object.amount + ' ' + time_object.unit + ', ';
+        }
+        seconds = time_object.remainingSeconds;
+    }
+    return formatText(converted_time);
 }
 
-function numberOf(unit_value, unit, seconds) {
+function convertTimeUnit(unit_value, unit, seconds) {
     if ((seconds > 0) && (seconds >= unit_value)) {
         return { amount: Math.floor(seconds / unit_value),
                  remainingSeconds: seconds % unit_value,
                  unit: unit + (Math.floor(seconds / unit_value) > 1 ? 's' : '') };
     } else {
-        return { amount: 0, remainingSeconds: seconds };
+        return { amount: 0,
+                 remainingSeconds: seconds,
+                 unit: '' };
     }
 }
 
-
-function separation() {
-    if (seconds > 0) {
-        return ', ';
+function formatText(converted_time) {
+    converted_time = converted_time.substring(0, converted_time.length - 2);
+    let last_comma = converted_time.lastIndexOf(', ');
+    if (last_comma > 0) {
+        return converted_time.slice(0, last_comma) + ' and ' + converted_time.slice(last_comma + 1);
+    } else {
+        return converted_time;
     }
 }
 
@@ -40,5 +46,5 @@ let convertButton = document.getElementById('convert');
 
 convertButton.onclick = function() {
     let secondsToConvert = secondsToConvertInput.value;
-    timeConverted.innerHTML = solution(secondsToConvert);
+    timeConverted.innerHTML = convert(secondsToConvert);
 };
